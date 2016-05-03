5918e06d840a715b7e93d485bb27f188132f307c
Nicholas G Reich <nick@schoolph.umass.edu>
2016-05-03 09:32:07 -0400

    updates to poseterior data and data documentation.

--- R/data-ebola_gamma_pstr.R
+++ R/data-ebola_gamma_pstr.R
@@ -4,15 +4,18 @@
 #' incubation period of Ebola. Distribution was estimated from de-identified
 #' data from Faye et al. (2015).
 #'
-#' @format A data frame with 1,700,000 rows and 2 variables:
+#' @format A data frame with 3,000,000 rows and 5 variables:
 #' \itemize{
 #'   \item \code{shape} shape parameter of gamma distribution
 #'   \item \code{scale} scale parameter of gamma distribution
+#'   \item \code{idx} index of sample
+#'   \item \code{median} the median correponding to this posterior sample
+#'   \item \code{p95} the 95th percentile correponding to this posterior sample
 #' }
 #' @docType data
 #' @references Faye, O. et al. Chains of transmission and control of Ebola virus disease in Conakry, Guinea, in 2014: an observational study. The Lancet Infectious Diseases 15, 320–326 (2015).
-#' @name ebola_gamma_pstr
-#' @usage data(ebola_gamma_pstr)
+#' @name ebola_gamma_posterior
+#' @usage data(ebola_gamma_posterior)
 NULL
 
 
@@ -23,13 +26,14 @@ NULL
 #' incubation period of MERS-CoV. Distribution was estimated from de-identified
 #' data from Virlogeux et al. (2016).
 #'
-#' @format A data frame with 5 variables:
+#' @format A data frame with 1,000,000 rows and 6 variables:
 #' \itemize{
 #'   \item \code{shape} shape parameter of gamma distribution
 #'   \item \code{scale} scale parameter of gamma distribution
 #'   \item \code{idx} index of sample
 #'   \item \code{median} the median correponding to this posterior sample
 #'   \item \code{p95} the 95th percentile correponding to this posterior sample
+#'   \item \code{chain} which chain the sample belongs to
 #'   }
 #' @docType data
 #' @references Virlogeux, V., Park, M., Wu, J. T. & Cowling, B. J. Association between Severity of MERS-CoV Infection and Incubation Period. Emerging Infectious Diseases 22, (2016).
@@ -45,13 +49,14 @@ NULL
 #' incubation period of smallpox. Distribution was estimated from de-identified
 #' data from several sources (see References).
 #'
-#' @format A data frame with 5 variables:
+#' @format A data frame with 1,000,000 rows and 6 variables:
 #' \itemize{
 #'   \item \code{shape} shape parameter of gamma distribution
 #'   \item \code{scale} scale parameter of gamma distribution
 #'   \item \code{idx} index of sample
 #'   \item \code{median} the median correponding to this posterior sample
 #'   \item \code{p95} the 95th percentile correponding to this posterior sample
+#'   \item \code{chain} which chain the sample belongs to
 #'   }
 #' @docType data
 #' @references 
