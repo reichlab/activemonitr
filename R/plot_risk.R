0d055519472895bdb60d5b411963aa06ff5a8e7e
Steve Lauer <stevelauer@Steves-MacBook-Pro.local>
2020-01-30 18:07:30 -0500

    all changes for plot_risk_uncertainty: replaced u documentation with max_u (which was changed before I started); allow users to choose a quantile for the long tail rather than force them to use 0.99 (which is now the default); also allow users to choose alpha and nreps; MERS code ran after all changes were made

--- R/plot_risk.R
+++ R/plot_risk.R
@@ -10,27 +10,27 @@
 #' @export
 #'
 #' @examples plot_risk(ebola_gamma_pstr, u=c(5, 10))
-plot_risk <- function(pstr_data, 
-                      u=7, 
+plot_risk <- function(pstr_data,
+                      u=7,
                       phi=c(.0001, .001, .01),
                       durations=5:25,
                       return_plot=FALSE) {
     require(ggplot2)
     require(grid)
-    dat <- expand.grid(shape=median(pstr_data$shape), 
-                       scale=median(pstr_data$scale), 
-                       u=u, 
+    dat <- expand.grid(shape=median(pstr_data$shape),
+                       scale=median(pstr_data$scale),
+                       u=u,
                        phi=phi,
                        d=durations)
     dat <- prob_of_missing_case(dat)
-    p <- ggplot(dat, aes(d, p, group=factor(phi), color=factor(phi))) + 
+    p <- ggplot(dat, aes(d, p, group=factor(phi), color=factor(phi))) +
         geom_line() + facet_grid(.~u) +
         scale_y_log10(breaks=c(1e-06, 1e-04, 1e-02), labels=c("1/1,000,000", "1/10,000", "1/100")) +
         scale_x_continuous(expand = c(0, 0)) +
-        ylab("probability of symptoms outside of active monitoring") + 
+        ylab("probability of symptoms outside of active monitoring") +
         xlab("duration of active monitoring (days)") +
         scale_color_discrete(name=expression(phi),
-                             breaks=c("0.01", "0.001", "1e-04"), 
+                             breaks=c("0.01", "0.001", "1e-04"),
                              labels=c("1/100", "1/1,000", "1/10,000")) +
         theme(panel.margin = unit(.7, "lines")) + ggtitle("stratified by days since exposure")
     print(p)
@@ -40,7 +40,10 @@ plot_risk <- function(pstr_data,
 #' Function for plotting risk and uncertainty of case escaping active monitoring
 #'
 #' @param pstr_data a data object with posterior distributions of gamma parameters (must have shape and scale column)
-#' @param u numeric value or vector of assumed duration(s) of time between exposure and monitoring
+#' @param long_tail_pct numeric value between 0 and 1, which percentile should be chosen to represent the long tail of the distribution
+#' @param alpha numeric value between 0 and 1, percentiles to use (with 1-alpha) when choosing the bounds for the shape and scale parameters
+#' @param nreps numeric, number of reps needed to stabilize uniform integration
+#' @param max_u numeric value, the maximum assumed duration of time between exposure and monitoring
 #' @param phi probability a case becomes symptomatic
 #' @param durations durations of active monitoring to plot
 #' @param yrange if not NULL, a vector of the ylim to plot
@@ -53,8 +56,11 @@ plot_risk <- function(pstr_data,
 #' @export
 #'
 #' @examples plot_risk_uncertainty(ebola_gamma_pstr)
-plot_risk_uncertainty <- function(pstr_data, 
-                                  max_u=14, 
+plot_risk_uncertainty <- function(pstr_data,
+                                  long_tail_pct=0.99,
+                                  alpha=0.05,
+                                  nreps=100,
+                                  max_u=14,
                                   phi=c(.0001, .001, .01),
                                   durations=5:25,
                                   yrange=NULL,
@@ -65,64 +71,64 @@ plot_risk_uncertainty <- function(pstr_data,
     require(ggplot2)
     require(grid)
     ## calculate 99th percentile of distributions, to pick confidence bounds
-    pstr_data <- pstr_data %>% 
-        mutate(p99 = qgamma(p=.99, shape=shape, scale=scale)) 
-    
+    pstr_data <- pstr_data %>%
+        mutate(long_tail_pct = qgamma(p=long_tail_pct, shape=shape, scale=scale))
+
     ## find percentiles of distributions, based on bounds
-    alpha <- 0.05
-    param_bounds <- arrange(pstr_data, p99)[round(nrow(pstr_data)*c(alpha, 1-alpha)),]
-    
-    ## make dataset 
-    nreps <- 100 ## reps needed to stabilize uniform integration
+    param_bounds <- arrange(pstr_data, long_tail_pct)[round(nrow(pstr_data)*c(alpha, 1-alpha)),]
+
+    ## make dataset
     dat_sim_pst_param <- expand.grid(shape = param_bounds$shape,
                                      scale = param_bounds$scale,
                                      phi=phi,
                                      d=durations,
                                      reps=1:nreps)
     dat_sim_pst_param$u <- sample(1:max_u, size=nrow(dat_sim_pst_param), replace=TRUE)
-    
+
     dat_sim_pst_param <- prob_of_missing_case(dat_sim_pst_param)
     dat_sim_pst_param_sum <- group_by(dat_sim_pst_param, d, phi) %>%
         summarize(p05 = quantile(p, prob=.05),
+                  p25 = quantile(p, prob=.25),
                   p50 = quantile(p, prob=.50),
+                  p75 = quantile(p, prob=.75),
                   p95 = quantile(p, prob=.95)) %>%
         ungroup()
-    
+
     ## something like this will make labels appear right
     if(identical(unique(dat_sim_pst_param_sum$phi), c(1e-04, 2e-03))) {
-        dat_sim_pst_param_sum$phi_lab <- factor(dat_sim_pst_param_sum$phi, 
-                                                levels=c(1e-04, 2e-03), 
-                                                labels=c("phi==1/10000", "phi==1/500")) 
+        dat_sim_pst_param_sum$phi_lab <- factor(dat_sim_pst_param_sum$phi,
+                                                levels=c(1e-04, 2e-03),
+                                                labels=c("phi==1/10000", "phi==1/500"))
     } else {
         ## for now, leaving out fancy formatting
         dat_sim_pst_param_sum$phi_lab <- factor(dat_sim_pst_param_sum$phi)
     }
-    p <- ggplot(dat_sim_pst_param_sum, aes(x=d, color=phi_lab, fill=phi_lab)) + 
+    p <- ggplot(dat_sim_pst_param_sum, aes(x=d, color=phi_lab, fill=phi_lab)) +
         #facet_grid(.~phi_lab, labeller=label_parsed) +
         facet_grid(.~phi_lab, labeller=label_parsed) +
-        geom_line(aes(y=p50)) + 
-        geom_ribbon(aes(ymin=p05, ymax=p95), alpha=.2, color=NA) + 
+        geom_line(aes(y=p50)) +
+        geom_ribbon(aes(ymin=p05, ymax=p95), alpha=.2, color=NA) +
         scale_y_log10() +
-        ylab("Pr(symptoms after AM)") + 
+        ylab("Pr(symptoms after AM)") +
         theme(legend.justification=c(0,0), legend.position=c(0,0))
-    
+
     ## formatting options for xlab
     if(include_xlab) {
-        p <- p + xlab("duration of active monitoring (days)") 
+        p <- p + xlab("duration of active monitoring (days)")
     } else {
-        p <- p + xlab(NULL) 
+        p <- p + xlab(NULL)
     }
-    
+
     ## remove legend if specified
     if(!include_legend) {
-        p <- p + guides(color=FALSE, fill=FALSE) 
+        p <- p + guides(color=FALSE, fill=FALSE)
     }
-    
+
     ## zoom on y axis if desired
     if(!is.null(yrange)) {
         p <- p + coord_cartesian(ylim=yrange)
     }
-    
+
     print(p)
     out <- vector("list", 2)
     names(out) <- c("data", "plot")
